dab3b1759c6615e29af5f9f79ac117adce525b1f
Sleeping_Forest <d.n.anonim@gmail.com>
2021-03-25 22:41:09 +0000

    1 Lesson

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//Задание №1
+let tC = prompt("Введите температуру в Целсиях:")
+const tF = (9/5)*tC+32;
+alert (tF);
+console.log ("Задача 1: Температура в Фаренгейтах: " + tF);
+// Конец задания №1
+
+//Задание №2
+let admin;
+let name="Василий";
+admin = name;
+alert ("Задача 2: Имя админа: " + admin);
+console.log ("Задача 2: Имя админа: " + admin);
+// Конец задания №2
+
+//Задание №3
+let digitValue = 1000;
+let textValue="108";
+let additionResult = digitValue + textValue;
+alert ("Задача 3: Результат выражения: " + additionResult);
+console.log ("Задача 3: Результат выражения: " + additionResult);
+// Конец задания №3
